f4bc03802484724b84ca176095f7dfc92ff690eb
mohammad jalifathi <m.h.jalifathi@gmail.com>
2024-01-02 19:33:18 +0330

    some changes

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -46,8 +46,7 @@ const SearchExercises = () => {
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
-      <Typography
-        fontWeight={700}
+      <Typography fontWeight={700}
         sx={{
           fontSize: { lg: "44px", xs: "30px" },
           mb: "50px",
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,21 +1,21 @@
 export const exerciseOptions = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '0922ef6996msh9d0797e9123a1e7p108bf6jsn212389b32809',
   },
 };
 
 export const youtubeOptions = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '0922ef6996msh9d0797e9123a1e7p108bf6jsn212389b32809',
   },
 };
 
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
-  const data =  response.json();
+  const data = response.json();
   return data;
 };
